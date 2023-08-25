73afa4ac3464e7a4eb8b7efa0e04f90ce4dba98c
SUMIT RAJ <sumitraj1533@gmail.com>
2023-08-25 07:52:32 +0530

    Update index.js
    Addition of MySQL database

--- server/index.js
+++ server/index.js
@@ -3,11 +3,36 @@ import {dirname} from 'path'
 import {fileURLToPath} from 'url'
 import path from 'path'
 import bodyParser from 'body-parser'
+import mysql from 'mysql2'
 
 const app=express()
 const port=3000
 const __dirname=dirname(fileURLToPath(import.meta.url))
-
+const connection = mysql.createConnection({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'root',
+    database:'feedback'
+});
+// create connection.
+connection.connect((err)=>{
+    if(err){
+        console.log("error");
+    }
+    else{
+        console.log('successful connection');
+    }
+});
+// End connection
+// connection.end((err)=>{
+//     if(err){
+//         console.log('error');
+//     }
+//     else{
+//         console.log('successful disconnected');
+//     }
+// })
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,19 +43,27 @@ app.get("/feedback",(req,res)=>{
     res.sendFile(path.join(__dirname,'feedback.html'))
 })
 
-app.post("/file",(req,res)=>{
-    const filename=req.body['file']
-    console.log(filename)
-    res.sendFile(path.join(__dirname,"homepage.html"))
-})
 app.post("/feedback",(req,res)=>{
     const name=req.body['name']
     const email=req.body['email']
     const feedback=req.body['feedback']
     console.log(name+" "+email+" "+feedback)
-    res.sendFile(path.join(__dirname,"homepage.html"))
+    const insert= 'INSERT INTO record(name,email,feed) values (?,?,?)';
+        connection.query(insert,[name,email,feedback],(err,result)=>{
+            if(err){
+                console.error("Error:- ",err);
+                return;
+            }
+            else{
+                console.log("Result :- ",result);
+            }
+        });
+    res.render('acknowledge.ejs',{name});
+    // res.sendFile(path.join(__dirname,"homepage.html"))
 })
-
+app.post('/return',(req,res)=>{
+    res.sendFile(path.join(__dirname,"homepage.html"));
+})
 app.listen(port,(req,res)=>{
     console.log("Port created")
 })
